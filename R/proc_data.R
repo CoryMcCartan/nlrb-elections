2c0540f692a5541ea5b107eb7e4eeaa11c942eac
Cory McCartan <cory.mccartan@gmail.com>
2021-04-08 23:57:04 -0400

    initial plot

--- R/proc_data.R
+++ R/proc_data.R
@@ -1,11 +1,13 @@
 library(here)
 library(tidyverse)
 library(janitor)
+library(lubridate)
 
 raw = read_csv(here("data-raw/recent_elections.csv"))
 
 d = clean_names(raw) %>%
     filter(status == "Closed",
+           str_detect(ballot_type, "Single"),
            is.na(labor_union2), is.na(labor_union3)) %>%
     select(-status, -matches("union[23]"), -matches("unit_unit_[abcd]"),
            -union_to_certify) %>%
@@ -19,7 +21,12 @@ d = clean_names(raw) %>%
            votes_counted=total_ballots_counted,
            votes_challenged=challenged_ballots,
            challenges_determ=challenges_are_determinative) %>%
+    filter(votes_yes <= votes_counted,
+           votes_no <= votes_counted,
+           votes_counted <= eligible) %>%
     select(-unit_id) %>%
-    mutate(across(c(case_name, city, union), str_to_upper))
+    mutate(across(c(case_name, city, union), str_to_upper),
+           across(contains("date"), mdy)) %>%
+    distinct()
 
 write_rds(d, here("data/one_union_elec.rds"), compress="xz")
---
+++ README.md
@@ -0,0 +1,9 @@
+# NLRB Elections Data and Analysis
+
+- `data/` includes some slightly cleaned-up (and heavily compressed) election
+data from the NLRB website (original available at 
+<https://www.nlrb.gov/reports/graphs-data/recent-election-results>)
+
+- Analysis and processing scripts in `R/`
+
+- Write-up in `writeup/`
