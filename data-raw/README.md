54aba87b88a3d65a1434bf8b645318adf702e68c
Cory McCartan <cory.mccartan@gmail.com>
2021-04-08 22:18:17 -0400

    init

---
+++ data-raw/README.md
@@ -0,0 +1,4 @@
+# Getting raw data
+
+Go to the [NLRB Recent Elections page](https://www.nlrb.gov/reports/graphs-data/recent-election-results)
+and press "Download CSV." This will give you the data for all elections since 2009.
---
+++ proc_data.R
@@ -0,0 +1,25 @@
+library(here)
+library(tidyverse)
+library(janitor)
+
+raw = read_csv(here("data-raw/recent_elections.csv"))
+
+d = clean_names(raw) %>%
+    filter(status == "Closed",
+           is.na(labor_union2), is.na(labor_union3)) %>%
+    select(-status, -matches("union[23]"), -matches("unit_unit_[abcd]"),
+           -union_to_certify) %>%
+    group_by(case) %>%
+    filter(length(unique(unit_id)) == 1) %>% #  one voting unit
+    ungroup() %>%
+    rename(eligible=no_of_eligible_voters,
+           union=labor_union1,
+           votes_yes=votes_for_labor_union1,
+           votes_no=votes_against,
+           votes_counted=total_ballots_counted,
+           votes_challenged=challenged_ballots,
+           challenges_determ=challenges_are_determinative) %>%
+    select(-unit_id) %>%
+    mutate(across(c(case_name, city, union), str_to_upper))
+
+write_rds(d, here("data/one_union_elec.rds"), compress="xz")
